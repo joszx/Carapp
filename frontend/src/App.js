844db3a2c5075910c9c56e1bf3bbef24b7c59142
joszx <joshua_rockz@hotmail.com>
2022-11-02 15:32:04 +0800

    Change background css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,18 @@
-import logo from "./logo.svg";
 import "./App.css";
+import "bulma/css/bulma.min.css";
+import ContactList from "./components/ContactList";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello World!</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="columns is-centered">
+          <div className="column is-half">
+            <h1 className="title has-text-white">My Contact List</h1>
+            <ContactList />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
